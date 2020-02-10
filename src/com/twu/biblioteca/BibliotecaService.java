09627ec8b302286d8ccdbc050584122499668608
anacarolina-f <anacarolina.f@thoughtworks.com>
2020-02-10 09:05:52 -0300

    refactoring services methods

--- src/com/twu/biblioteca/BibliotecaService.java
+++ src/com/twu/biblioteca/BibliotecaService.java
@@ -9,6 +9,7 @@ public class BibliotecaService {
     private BibliotecaMenu menu = new BibliotecaMenu();
     private BookService bookService = new BookService();
     private Scanner scanner = new Scanner(System.in);
+    private int bookId;
     private static final String WELCOME_MSG = "Welcome to Biblioteca. Your on-stop-shop for great book titles in Bangalore";
     private static final String ONLY_NUM = "Only numbers allowed.";
     private static final String QUIT = "Quit.";
@@ -17,72 +18,77 @@ public class BibliotecaService {
     public BibliotecaService() { }
 
     public void start() {
-        System.out.println(welcomeMessage());
-        System.out.println(showMainMenu());
+        System.out.println(menuStart());
 
         try {
             int option;
             do {
-                option = scanner.nextInt();
+                option = userInput();
                 showMenuOption(option);
             } while (option != 0);
-
         } catch (InputMismatchException e) {
             System.out.println(ONLY_NUM);
         }
     }
 
     private void showMenuOption(int option) {
-        int bookID;
         switch (option) {
             case 1:
-                System.out.println(showBookList());
-                System.out.println(showMainMenu());
+                showListBooks();
                 break;
             case 2:
-                System.out.println(showMenuCheckout());
-                bookID = scanner.nextInt();
-                checkoutBook(bookID);
-                System.out.println(showMainMenu());
+                menuCheckoutBook();
                 break;
             case 3:
-                System.out.println(showReturnMenu());
-                bookID = scanner.nextInt();
-                returnBook(bookID);
-                System.out.println(showMainMenu());
+                menuReturnBook();
                 break;
             case 0:
                 System.out.println(QUIT);
                 break;
             default:
-                System.out.println(INVALID);
-                System.out.println(showMainMenu());
+                menuInvalidOption();
                 break;
         }
     }
 
-    private String welcomeMessage() {
-        return WELCOME_MSG;
+    private void showListBooks() {
+        System.out.println(showBookList());
+        System.out.println(menu.showMainMenu());
     }
 
-    private String showMainMenu() {
-        return menu.showMainMenu();
+    private void menuCheckoutBook() {
+        System.out.println(menu.showCheckoutMenu());
+        bookId = userInput();
+        checkoutBook(bookId);
+        System.out.println(menu.showMainMenu());
+    }
+
+    private void menuReturnBook() {
+        System.out.println(menu.showReturnMenu());
+        bookId = userInput();
+        returnBook(bookId);
+        System.out.println(menu.showMainMenu());
+    }
+
+    private int userInput() {
+        return scanner.nextInt();
+    }
+
+    private void menuInvalidOption() {
+        System.out.println(INVALID);
+        System.out.println(menu.showMainMenu());
+    }
+
+    private String menuStart() {
+        return WELCOME_MSG + "\n" + menu.showMainMenu();
     }
 
     private String showBookList() {
         return bookService.getListAvailableBooks().stream().collect(Collectors.joining());
     }
 
-    private String showMenuCheckout() {
-        return menu.showCheckoutMenu();
-    }
-
     private void checkoutBook(int bookID) {
         System.out.println(bookService.checkoutBook(bookID) + "\n");
-    }
-
-    private String showReturnMenu() {
-        return menu.showReturnMenu();
     }
 
     private void returnBook(int bookID) {
